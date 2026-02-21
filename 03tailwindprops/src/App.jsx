48678306e71a99000058f2ab6a5c32e656981bcd
Neeraj Bhayal <neerajbhayal@Neerajs-MacBook-Air.local>
2026-02-21 11:10:07 +0530

    Add existing project files

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <>
+       <h1 className='bg-green-400 rounded-2xl p-5 text-black '>neeraj bhayal</h1>
+        <Card  name="Neeraj"/>
+        <Card  name="Preeti"/>
+    </>
+  )
+}
+
+export default App
