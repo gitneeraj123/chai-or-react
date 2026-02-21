0b4725569de60cce644a1b55ad20a2ac16cd90b3
Neeraj Bhayal <neerajbhayal@Neerajs-MacBook-Air.local>
2026-02-21 15:14:28 +0530

    Add existing project files

--- 02counter/counter/src/App.jsx
+++ 02counter/counter/src/App.jsx
@@ -1,13 +1,14 @@
-import {useState,useEffect} from 'react'
+import {useState} from 'react'
 import './App.css'
 
 
 function App() {
-
   let [counter,setCounter] = useState(5);
 
   const addValue = ()=>{
       ++counter;
+      if(counter<=20) setCounter(counter);
+      if(counter<=20) setCounter(counter);
       if(counter<=20) setCounter(counter);
   }
 
@@ -24,5 +25,33 @@ function App() {
       <button onClick={removeValue} >Decrease Value: {counter}</button> <br />
     </>
   )
-}
-export default App
+  }
+
+
+  //Diffing algorithm for list rendering using unique keys ;
+  // const fruits = [
+  //   { id: 101, name: "Apple", price: "$2", color: "Red" },
+  //   { id: 102, name: "Banana", price: "$1", color: "Yellow" },
+  //   { id: 103, name: "Grape", price: "$4", color: "Purple" }
+  // ];
+  // function App() {
+  //   // A simple function to handle the button click
+  //   const buyFruit = (name) => {
+  //     alert(`You added ${name} to your cart!`);
+  //   };
+  //   return (
+  //       <div>
+  //         <h1>Fruit Store</h1>
+  //         {fruits.map((fruit)=>(
+  //           <div key={fruit.id} style={{backgroundColor: }} >
+  //               <p>{fruit.name}</p>
+  //               <p>{fruit.price}</p>
+  //               <p style={{color: fruit.color.toLower}} >fruit.color</p>
+  //               <button onClick={()=>buyFruit(fruit.name)}>Buy {fruit.name}</button>
+  //           </div>
+  //         ))}
+  //       </div>
+  //   )}
+
+
+  export default App
---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive")
+  return (
+    <div className='h-screen w-full duration-200' style={{backgroundColor:color}}>
+       <div className='flex flex-wrap gap-1 justify-between bg-white rounded-3xl p-3'>
+        <button onClick={()=>setColor("red")} className='bg-red-800 rounded-2xl text-white p-1 hover:'>Red</button>
+        <button onClick={()=>setColor("green")} className='bg-green-800 rounded-2xl text-white p-1'>Green</button>
+        <button onClick={()=>setColor("blue")} className='bg-blue-800 rounded-2xl text-white p-1'>Blue</button>
+        <button onClick={()=>setColor("olive")} className='bg-olive-800 rounded-2xl text-white p-1'>Olive</button>
+        <button onClick={()=>setColor("gray")} className='bg-gray-800 rounded-2xl text-white p-1'>Gray</button>
+        <button onClick={()=>setColor("red")} className='bg-yellow-800 rounded-2xl text-white p-1' >Yellow</button>
+        <button onClick={()=>setColor("pink")} className='bg-pink-800 rounded-2xl text-white p-1' >Pink</button>
+        <button onClick={()=>setColor("purple")} className='bg-purple-800 rounded-2xl text-white p-1'>Purple</button>
+        <button onClick={()=>setColor("lime")} className='bg-lime-800 rounded-2xl text-white p-1' >Lime</button>
+        <button onClick={()=>setColor("white")} className='bg-slate-100 border border-gray-300 rounded-2xl text-black p-1' >White</button>
+        <button onClick={()=>setColor("black")} className='bg-black rounded-2xl text-white p-1' >Black</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
